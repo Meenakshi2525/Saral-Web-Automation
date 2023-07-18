de6b73e08cfcc10a6268dfc27d995b985e54ab5c
Meenakshi <meenakshi.sagar@jarvis.consulting>
2023-07-18 11:53:24 +0530

    changes for automation

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"5331e2d1-6d87-4191-8e2d-2aebe3da969d","feature":"Testing data entry for ward level karyakarta","scenario":"Testing ward karyakarta add data and edit entry functionality","start":1689655120120,"group":1,"content":"","tags":"","end":1689655317041,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"f28bde10-efa6-4df8-9c47-8256f41a3057","feature":"Testing data entry for ward level karyakarta","scenario":"Testing ward karyakarta add data and edit entry functionality","start":1689660770896,"group":1,"content":"","tags":"","end":1689660962518,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
 });
